984627e41152df8f69ee725c9b93a5ce28d82ecf
leockio <70870773+leockio@users.noreply.github.com>
2022-12-04 10:35:20 +0000

    Deploying to gh-pages from @ leockio/bubk@6e58da122805e33b21a2a1ad6f266aa26fdd7ad9 🚀

--- assets/pageview.62f7ade8.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./app.00a870ed.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
---
+++ assets/pageview.677577d7.js
@@ -0,0 +1,1 @@
+import{i as t}from"./app.ef367bf2.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
